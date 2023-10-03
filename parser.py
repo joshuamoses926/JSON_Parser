1ac3b4f25061ebfa0549173e8e272a506e03a62d
Joshua Moses <79341086+joshuamoses926@users.noreply.github.com>
2023-10-03 17:01:00 -0500

    Update parser.py

--- parser.py
+++ parser.py
@@ -14,7 +14,7 @@ for root, dirs, files in os.walk(directory):
           if filename.endswith('.json'):
             #construct the path to reach the JSON
             file_path = os.path.join(root, filename)
-            print(f'Parsing {file_path} \n')
+            print(f'Parsing {file_path} ')
 
             #opening the JSON files
             try:
@@ -22,7 +22,7 @@ for root, dirs, files in os.walk(directory):
                 with open(file_path) as file:
                     #Parse the file
                     data = json.load(file)
-                    print(f'Parsed JSON from {filename}: {data}')
+                    print(f'Parsed JSON from {filename}: {data} \n')
             except json.JSONDecodeError as e:
                 #Handle decoding errors in invalid JSON files
-                print(f'Error Parsing {filename}: {str(e)}')
+                print(f'Error Parsing {filename}: {str(e)} \n')
